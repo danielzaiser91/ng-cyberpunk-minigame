093c2ccb55c51a7caef85d4a2832bbcf77692c2a
Daniel <danielzaiser91@googlemail.com>
2020-12-17 00:56:29 +0100

    add stuff

--- src/app/code-matrix/code-matrix.component.ts
+++ src/app/code-matrix/code-matrix.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./code-matrix.component.sass']
 })
 export class CodeMatrixComponent implements OnInit {
-  // 6x6 matrix, 5 verschiedene zeichen, kombination von 2-4, muss möglich sein
+  // 6x6 matrix, 6 verschiedene zeichen, kombination muss möglich sein
   exampleMatrix = [
     ["E9", "E9", "BD", "1C", "55", "55"],
     ["E9", "E9", "BD", "1C", "55", "55"],
@@ -15,37 +15,65 @@ export class CodeMatrixComponent implements OnInit {
     ["E9", "E9", "BD", "1C", "55", "55"],
     ["E9", "E9", "BD", "1C", "55", "55"]
   ]
-  possibleValues = ["E9", "BD", "1C", "55", "7A"]
+  matrix: string[][] = [];
+  possibleValues = ["E9", "BD", "1C", "55", "7A", "FF"]
   toggleXY = false;
   hoveredId = 0;
   buffer: string[] = ["","","",""];
-  bufferClean: string[] = ["","","",""];
+  bufferClean: string[] = [];
   maxBuffer = 4;
-  solution: string[] = ["E9", "BD", "1C"];
+  solution: string[][] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.randomize();
   }
 
+  deck(size: number) {
+    this.maxBuffer = size;
+    this.buffer = [];
+    for(let i = 0; i < this.maxBuffer; i++) this.buffer.push("");
+  }
+
+  randomize() {
+    for(let i = 0; i < 6; i++) {
+      for(let y = 0; y < 6; y++) {
+        if(y == 0) this.matrix[i] = [];
+        this.matrix[i].push(this.possibleValues[this.rand(6)]);
+      }
+    }
+    for(let i = 0; i < 2; i++) {
+      for(let y = 0; y < 3; y++) {
+        if(y == 0) this.solution[i] = [];
+        this.solution[i].push(this.possibleValues[this.rand(6)]);
+      }
+    }
+  }
   clickedElement(el: any) {
     this.toggleXY = !this.toggleXY
+    this.buffer.push(el.target.innerText);
     this.bufferClean = this.buffer.filter(x=>!!x);
     this.buffer = [...this.bufferClean];
-    this.buffer.push(el.target.innerText);
     const length = this.buffer.length;
-    for(let i = 0; i <= (this.maxBuffer - length); i++) this.buffer.push("");
+    for(let i = 0; i < (this.maxBuffer - length); i++) this.buffer.push("");
     if(this.buffer.length > this.maxBuffer) this.buffer.pop();
   }
   reset() {
-    this.buffer = ["","","",""]
+    this.buffer = ["","","",""];
+    this.bufferClean = [];
+    this.randomize();
   }
-  rand() {
-    return Math.floor(Math.random() * 10);
+  rand(range: number, startAt = 0) {
+    return (Math.floor(Math.random() * 10) % range) + startAt;
   }
-  correct(){
-    return this.bufferClean.toLocaleString().includes(this.solution.toLocaleString())
+  correct(nr: number){
+    if (this.bufferClean.length == 0) return false;
+    if(nr == 1) return this.bufferClean.toLocaleString().includes(this.solution[0].toLocaleString())
+    if(nr == 2) return this.bufferClean.toLocaleString().includes(this.solution[1].toLocaleString())
+    return false;
   }
-  corrected(){
-    console.log(this.bufferClean, this.solution, this.correct());
+  log(){
+    console.log(this.rand(10));
   }
 }
